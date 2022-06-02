02c91fab47127631f81d7bcc62f3ff7805ee25b8
b217ck <b217ck@gmail.com>
2022-06-02 14:54:08 +0400

    Добавил комментарии, убрал ненужные строки

--- log.py
+++ log.py
@@ -1,16 +1,22 @@
+#подключение модуля для поиска
 import re
+#подключение модуля красивой печати
 from pprint import pprint
 
+#функция поиска по нужному ip, поиска sid и сортировки найденных значений
 def parse(filename):
     res = {}
-    i = 0
+    #переменная для поиска нужного ip
     ipfind = '10.1.192.38'
+    #открываем файл и ищем sid в строке, которая начинается на ip
     with open(filename) as f:
         for line in f:
             if line.startswith(ipfind):
                 sid = re.search(r'sid=/(\S+)/', line).group(1)
                 res[sid] = ipfind
     return res
-
+#выводим описание
 print('Отсортированные sid с ip 10.1.192.38:')
+#запускаем функцию с нужным файлом и печатаем результат
 pprint(parse('log.txt'))
+# ??? PROFIT
